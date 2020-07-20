bba99d6bbccc70ddd144c3cc674d20485b12b871
Ian Mayton <ian.mayton@gmail.com>
2020-07-20 18:33:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,10 @@ def first_challenge
          age: "15",
          status: "alive"
        },
-       :hero_friends => []
+       :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"}
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         ]
      },
      
      :capulet => {
@@ -32,7 +35,10 @@ def first_challenge
          age: "15",
          status: "alive"
        },
-       :heroine_friends => []
+       :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"}
+         {name: "Nurse", age: "44", attitude: "worried"}
+         ]
      }
    }
 end
